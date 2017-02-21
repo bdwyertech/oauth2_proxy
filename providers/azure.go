7cb4d52e944a04605eca7a5f4ff6556938cf9d66
Brian Dwyer <bdwyer@IEEE.org>
2017-02-20 21:55:38 -0500

    Give Refresh tokens a shot

--- providers/azure.go
+++ providers/azure.go
@@ -1,14 +1,19 @@
 package providers
 
 import (
+  "bytes"
 	"errors"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"github.com/bitly/go-simplejson"
 	"github.com/bitly/oauth2_proxy/api"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
 
 type AzureProvider struct {
@@ -231,4 +236,185 @@ func (p *AzureProvider) ValidateGroup(s *SessionState) bool {
 
 func (p *AzureProvider) ValidateSessionState(s *SessionState) bool {
 	return validateToken(p, s.AccessToken, getAzureHeader(s.AccessToken))
+}
+
+//
+// Pull Email from ID Token
+//
+func upnFromIdToken(idToken string) (string, error) {
+
+	// id_token is a base64 encode ID token payload
+	// https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-oauth-code#jwt-token-claims
+	jwt := strings.Split(idToken, ".")
+	b, err := jwtDecode(jwt[1])
+	if err != nil {
+		return "", err
+	}
+
+	var upn struct {
+		Upn         string `json:"upn"`
+	}
+	err = json.Unmarshal(b, &upn)
+	if err != nil {
+		return "", err
+	}
+	if upn.Upn == "" {
+		return "", errors.New("missing UPN")
+	}
+	return upn.Upn, nil
+}
+
+//
+// Java Web Token Decoder
+//
+func jwtDecode(seg string) ([]byte, error) {
+	if l := len(seg) % 4; l > 0 {
+		seg += strings.Repeat("=", 4-l)
+	}
+
+	return base64.URLEncoding.DecodeString(seg)
+}
+
+//
+// Token Redemption
+//
+func (p *AzureProvider) Redeem(redirectURL, code string) (s *SessionState, err error) {
+	if code == "" {
+		err = errors.New("missing code")
+		return
+	}
+
+	params := url.Values{}
+	params.Add("redirect_uri", redirectURL)
+	params.Add("client_id", p.ClientID)
+	if p.ClientSecret != "" {
+		params.Add("client_secret", p.ClientSecret)
+	}
+	params.Add("code", code)
+	params.Add("grant_type", "authorization_code")
+	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
+		params.Add("resource", p.ProtectedResource.String())
+	}
+
+	var req *http.Request
+	req, err = http.NewRequest("POST", p.RedeemURL.String(), bytes.NewBufferString(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("got %d from %q %s", resp.StatusCode, p.RedeemURL.String(), body)
+		return
+	}
+
+	var jsonResponse struct {
+		AccessToken  string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresOn    int64  `json:"expires_on,string"`
+		IdToken      string `json:"id_token"`
+	}
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		return
+	}
+	var email string
+	email, err = upnFromIdToken(jsonResponse.IdToken)
+	if err != nil {
+		return
+	}
+
+	s = &SessionState{
+		AccessToken:  jsonResponse.AccessToken,
+		ExpiresOn:    time.Unix(jsonResponse.ExpiresOn, 0),
+		// Refresh Token makes the cookie too large
+		// RefreshToken: jsonResponse.RefreshToken,
+		Email:        email,
+	}
+	return
+}
+
+//
+// Session Refresh
+//
+func (p *AzureProvider) RefreshSessionIfNeeded(s *SessionState) (bool, error) {
+	if s == nil || s.ExpiresOn.After(time.Now()) || s.RefreshToken == "" {
+		return false, nil
+	}
+
+	newToken, newRefresh, duration, err := p.redeemRefreshToken(s.RefreshToken)
+	if err != nil {
+		return false, err
+	}
+
+	// re-check that the user is in the proper Azure group(s)
+	// if !p.ValidateGroup(s) {
+	// 	return false, fmt.Errorf("%s is no longer in the group(s)", s.Email)
+	// }
+
+	origExpiration := s.ExpiresOn
+	s.AccessToken = newToken
+	s.RefreshToken = newRefresh
+	s.ExpiresOn = time.Now().Add(duration).Truncate(time.Second)
+	log.Printf("refreshed access token %s (expired on %s)", s, origExpiration)
+	return true, nil
+}
+
+//
+// Refresh Token
+//
+func (p *AzureProvider) redeemRefreshToken(refreshToken string) (token string, refresh string, expires time.Duration, err error) {
+	// https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-oauth-code#refreshing-the-access-tokens
+	params := url.Values{}
+	params.Add("client_id", p.ClientID)
+	params.Add("client_secret", p.ClientSecret)
+	params.Add("refresh_token", refreshToken)
+	params.Add("grant_type", "refresh_token")
+	var req *http.Request
+	req, err = http.NewRequest("POST", p.RedeemURL.String(), bytes.NewBufferString(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("got %d from %q %s", resp.StatusCode, p.RedeemURL.String(), body)
+		return
+	}
+
+	var data struct {
+		AccessToken string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn   int64  `json:"expires_in,string"`
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return
+	}
+	token = data.AccessToken
+	refresh = data.RefreshToken
+	expires = time.Duration(data.ExpiresIn) * time.Second
+	return
 }
